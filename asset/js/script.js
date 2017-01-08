3593e4e12e734690b95894a67098fad166927fa5
Yoann Di Maria <dm.yoann@gmail.com>
2017-01-08 20:02:15 +0100

    javascript

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,52 +1,99 @@
-function setCookie(name,value,days) {
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000));
-        var expires = "; expires="+date.toGMTString();
-    }
-    else var expires = "";
-    document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
-
-function eraseCookie(name) {
-    setCookie(name,"",-1);
-}
-
-
-
 $(document).ready(function() {
-    var idShoe = $('#shoe').attr('content');
-    /* change choose select */
+
+// function Cookie (create, edit, delete)
+    // function create Cookie
+    function setCookie(name,value,days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime()+(days*24*60*60*1000));
+            var expires = "; expires="+date.toGMTString();
+        }
+        else var expires = "";
+        document.cookie = name+"="+value+expires+"; path=/";
+    }
+    
+    // function edit Cookie
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+
+    // function delete(name)
+    function eraseCookie(name) {
+        setCookie(name,"",-1);
+    }
+
+// variable
+    var _idShoe = $('#shoe').attr('content'),
+    _colorShoe = $('.choose').attr('content');
+
+// change shoe picture
     $('.choose').click(function() {
         if($(this).attr('id') == 'shoe-1') {
             $('#shoe').css('background-image', 'url("asset/img/nike/nike_shoes.png")');
-        } else if($(this).attr('id') == 'shoe-2') {
+        }
+        else if($(this).attr('id') == 'shoe-2') {
             $('#shoe').css('background-image', 'url("asset/img/nike/nike_shoes_black.png")');
         }
+        alert(_colorShoe);
     });
     
-    /* click fav */
+// add article to fav
     $('#fav').click(function() {
-        if(getCookie('favShoes') == idShoe) {
+        if(getCookie('favShoes') == _idShoe) {
             eraseCookie('favShoes');
             alert('no : '+getCookie('favShoes'));
         }
-        else if(getCookie('favShoes') != idShoe) {
-            setCookie('favShoes', idShoe, 30);
+        else if(getCookie('favShoes') != _idShoe) {
+            setCookie('favShoes', _idShoe, 30);
             alert('ok : '+getCookie('favShoes'));
         }
     });
+    
+// header search
+    $('li a#search').hover(
+        function() {
+            $(this).append($('<input name="search" id="search" type="text" placeholder="Search a shoe">'));
+        },
+        function() {
+            $(this).find('input:last').remove();
+        }
+    );
+
+
+// scroll box
+    function ScrollCBox() {
+        var scrollbox = $('#scroll-container-box'),
+        t = parseInt(255)+parseInt($('body').scrollTop());
+        if ($('body').scrollTop() < 600) {
+            scrollbox.css({
+                top: '814px'
+            });
+        }
+        else if ($('body').scrollTop() > 600 && $('body').scrollTop() < 1200) {
+            scrollbox.css({
+                top: t+'px'
+            });
+            var t = parseInt(255)+parseInt($('body').scrollTop());
+            console.log(t+'px');
+        }
+        else if($('body').scrollTop() > 1200) {
+            scrollbox.css({
+                top: '1500px'
+            });
+        }
+    }
+    $(window).scroll(ScrollCBox);
+    ScrollCBox();
+
+// add article to cart
+
     $('#cart').click(function() {
         
         var d = new Date();
